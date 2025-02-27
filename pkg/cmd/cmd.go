7239575fffce90248a64ce45f2b471b58ef814ad
Veikko <kiis.veikko@gmail.com>
2025-02-27 14:49:31 +0200

    syntax optimization

---
+++ pkg/cmd/cmd.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+	"vk-books/pkg/db"
+)
+
+func CommandLine(books *db.Books) {
+
+	var userInput string = ""
+	var userInputID int = 0
+
+	fmt.Print("=> ")
+
+	fmt.Scanln(&userInput, &userInputID)
+
+	userInput = strings.ToLower(userInput)
+
+	switch userInput {
+	case "a", "add", "insert":
+		id := books.GenerateUniqueID()
+		newBook := db.GetUserInput(id)
+		books.Insert(newBook)
+	case "u", "update":
+		// Update
+	case "d", "delete":
+		// Delete
+	}
+
+}
